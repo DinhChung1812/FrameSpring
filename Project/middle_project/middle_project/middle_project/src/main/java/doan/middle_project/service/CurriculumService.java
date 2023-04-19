bff8c7148b23fc77a8f000eb45791c2a99280f1b
ngockien4102 <95838267+ngockien4102@users.noreply.github.com>
2023-04-20 01:02:35 +0700

    Delete CurriculumService.java

--- Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/service/CurriculumService.java
+++
@@ -1,8 +0,0 @@
-package doan.middle_project.service;
-
-
-import doan.middle_project.dto.Requests.CurriculumRequest;
-
-public interface CurriculumService {
-    void createCurriculum(CurriculumRequest curriculumRequest);
-}
