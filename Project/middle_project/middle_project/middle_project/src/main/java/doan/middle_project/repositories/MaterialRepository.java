ae8f771d3115ce39eace2918d46027c4391ff13a
kien <nguyenngockien41020@gmail.com>
2023-04-23 22:15:54 +0700

    update material get list, detail

---
+++ Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/repositories/MaterialRepository.java
@@ -0,0 +1,9 @@
+package doan.middle_project.repositories;
+
+import doan.middle_project.entities.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialRepository extends JpaRepository<Material,Integer> {
+}
