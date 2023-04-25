ff238038427d771674279a7bbc9caeaab0abe5d0
DinhChung1812 <xomduanb1812@gmail.com>
2023-04-25 22:59:26 +0700

    DinhChung: getAllElective

--- Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/controller/ElectiveController.java
+++ Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/controller/ElectiveController.java
@@ -1,36 +1,26 @@
 package doan.middle_project.controller;
 
-import doan.middle_project.common.vo.CuriculumVo;
-import doan.middle_project.common.vo.MessageVo;
-import doan.middle_project.common.vo.PLOVo;
-import doan.middle_project.common.vo.POVo;
-import doan.middle_project.dto.Requests.AssessmentRequest;
-import doan.middle_project.dto.Requests.CuriculumEditRequest;
-import doan.middle_project.dto.Requests.CurriculumRequest;
 import doan.middle_project.dto.Requests.ElectiveRequest;
-import doan.middle_project.service.CuriculumService;
 import doan.middle_project.service.ElectiveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ElectiveController {
     @Autowired
     ElectiveService electiveService;
 
-    @GetMapping("/get_all_elective")
+    @GetMapping("/get_all_subject_elective")
 //    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
-    public ResponseEntity<?> getAllElective(@RequestParam(required = false) String codeCurriculum) {
-        return electiveService.getAllElective(codeCurriculum);
+    public ResponseEntity<?> getAllSubjectByElectiveId(@RequestParam(required = false) Integer electiveId) {
+        return electiveService.getAllSubjectByElectiveId(electiveId);
     }
 
-    @GetMapping("/get_elective_by_id")
+    @GetMapping("/get_elective_by_curriCode")
 //    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
-    public ResponseEntity<?> getElectiveById(@RequestParam("elective_id") Integer electiveId) {
-        return electiveService.getElectiveById(electiveId);
+    public ResponseEntity<?> getElectiveByCurriCode(@RequestParam(required = false) String curriCode) {
+        return electiveService.getElectiveByCurriCode(curriCode);
     }
 
     @PutMapping("/update_insert_elective")
--- Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/entities/Elective.java
+++ Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/entities/Elective.java
@@ -27,15 +27,9 @@ public class Elective {
     @Column(name = "status")
     private Integer status;
 
-//    @ManyToOne
-//    @JoinColumn(name = "curriculum_id", referencedColumnName = "curriculum_id", nullable = false)
-//    private Curriculum curriculum;
-
     @ManyToMany (mappedBy = "electiveId",cascade = {CascadeType.MERGE})
     private Set<Curriculum> curriculumId;
 
-    @ManyToOne
-    @JoinColumn(name = "subject_id", referencedColumnName = "subject_id", nullable = false)
-    private Subject subject;
-
+    @OneToMany( mappedBy = "elective",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Subject> subject;
 }
