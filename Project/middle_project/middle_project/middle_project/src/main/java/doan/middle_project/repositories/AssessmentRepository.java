659fb06bb5f0996de1e41ecd1e4e5e7bf924ed13
BanCntt <bancntt@datlv.onmicrosoft.com>
2023-04-27 00:25:13 +0700

    elective

--- Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/repositories/AssessmentRepository.java
+++ Project/middle_project/middle_project/middle_project/src/main/java/doan/middle_project/repositories/AssessmentRepository.java
@@ -12,12 +12,13 @@ import java.util.List;
 
 @Repository
 public interface AssessmentRepository extends JpaRepository<Assessment, Integer> {
-    @Query(value = "select a.assessment_id,ac.assessment_cate_name , a.type, a.part, a.weight, a.completion_criteria, a.duration, a.question_type, a.question_no, a.knowledge_skill, a.grading_guide, a.note, ac.assessment_cate_id, s.syllabus_id\n" +
+    @Query(value = "select a.assessment_id,ac.assessment_cate_name , a.type, a.part, a.weight, \n" +
+            "a.completion_criteria, a.duration, a.question_type, a.question_no, a.knowledge_skill, \n" +
+            "a.grading_guide, a.note, ac.assessment_cate_id, s.syllabus_id\n" +
             "from assessment a \n" +
             "join assessment_category ac on a.assessment_cate_id = ac.assessment_cate_id\n" +
             "join syllabus_assessment sa on a.assessment_id = sa.assessment_id\n" +
             "join syllabus s on sa.syllabus_id = s.syllabus_id \n" +
-            "join subject su on s.subject_id = s.subject_id\n" +
             "where s.syllabus_id = ?1",nativeQuery = true)
     public List<Object[]> getAssessmentBySubject(Integer syllabusId);
 
